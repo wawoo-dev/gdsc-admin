558cce47e642b87abcaf2b73a6ac8c68352f84e1
NOGUEN <nhg1113@icloud.com>
2025-10-18 18:24:20 +0900

    chore: MobileLayout에 대한 설명 주석 추가 #169

--- src/components/@layout/MobileLayout.tsx
+++ src/components/@layout/MobileLayout.tsx
@@ -6,6 +6,24 @@ interface MobileLayoutProps {
   children: ReactNode;
 }
 
+/**
+ * 모바일 레이아웃 컴포넌트
+ *
+ * 최대 너비를 768px로 제한하여 대부분의 모바일 기기에 적합한 레이아웃을 제공
+ *
+ * @param props - 컴포넌트 props
+ * @param props.header - 상단에 표시될 헤더 컴포넌트 (선택사항)
+ * @param props.children - 본문에 표시될 자식 컴포넌트
+ *
+ * @returns 모바일 최적화된 레이아웃 컴포넌트
+ *
+ * @example
+ * ```tsx
+ * <MobileLayout header={<Header />}>
+ *   <MainContent />
+ * </MobileLayout>
+ * ```
+ */
 export default function MobileLayout({ header, children }: MobileLayoutProps) {
   return (
     <StyledLayoutWrapper>
@@ -15,6 +33,7 @@ export default function MobileLayout({ header, children }: MobileLayoutProps) {
   );
 }
 
+// 전체 레이아웃 구성
 const StyledLayoutWrapper = styled(Stack)({
   minHeight: "100vh",
   flexDirection: "column",
@@ -22,6 +41,7 @@ const StyledLayoutWrapper = styled(Stack)({
   width: "100%",
 });
 
+// 상단 고정 헤더 영역
 const StyledHeaderWrapper = styled("header")({
   "position": "sticky",
   "top": 0,
@@ -35,6 +55,7 @@ const StyledHeaderWrapper = styled("header")({
   },
 });
 
+// 본문 영역
 const StyledBodyWrapper = styled(Stack)({
   "flex": 1,
   "width": "100%",
